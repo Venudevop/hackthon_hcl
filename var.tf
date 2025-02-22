bcffe9003266c2da53ea9587e3840e30d3eeecc9
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-22 02:10:19 -0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,1 +1,11 @@
 
+################################ we can define variables here#####################################
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "bucket" {
+  type = string
+  default = "venu_hcl_hackthon"
+}
