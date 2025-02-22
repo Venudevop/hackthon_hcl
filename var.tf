6d78ce3b3ebcc934768a36e6d8e8479cadc14166
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-22 02:11:31 -0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,11 +1,14 @@
 
 ################################ we can define variables here#####################################
 variable "region" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  description = "The AWS region to deploy resources"
+  default     = "us-east-1"
 }
 
 variable "bucket" {
-  type = string
-  default = "venu_hcl_hackthon"
+  type        = string
+  description = "The name of the S3 bucket"
+  default     = "venu_hcl_hackthon"
 }
+
