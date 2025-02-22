700637f4e601663547cf53f176adc49d3115a5e6
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-22 04:35:44 -0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,9 +6,5 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "bucket" {
-  type        = string
-  description = "The name of the S3 bucket"
-  default     = "venu_hcl_hackthon"
-}
+
 
