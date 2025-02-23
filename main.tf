f519492cb07526946fc427dee6d68f08f5b152cd
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-23 07:58:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 provider "aws" {
 region = "var.region"   
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-terraform-backend-bucket"
+}
+
+resource "aws_dynamodb_table" "lock_table" {
+  name         = "my-terraform-lock-table"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-backend-bucket"    # S3 Bucket Name
+    key            = "path/to/my/terraform.tfstate"   # Path in S3 where state is stored
+    region         = "ap-south-1a"                       # AWS Region
+    encrypt        = true                              # Enable encryption of state file
+    dynamodb_table = "my-terraform-lock-table"        # DynamoDB Table for state locking
+    
+  }
+}
+
